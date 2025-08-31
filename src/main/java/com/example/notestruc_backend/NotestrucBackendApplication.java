f49980ea1b92079c9c9a84f77e049e0b1b4f5e3a
zh-deng <heromemo@web.de>
2025-08-31 15:56:54 +0200

    init commit

---
+++ src/main/java/com/example/notestruc_backend/NotestrucBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.notestruc_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotestrucBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotestrucBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/notestruc_backend/NotestrucBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.notestruc_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotestrucBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
